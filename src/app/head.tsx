add52486a2e31b0ec269f39ea8e7b02bd88c4064
miffybunnyrabbit <miffybunnyrabbit@gmail.com>
2025-03-19 08:09:56 +1100

    added head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>My App Title</title>
+        <meta name="description" content="This is my app" />
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content=":https://i.imgur.com/33rSf59.png" />
+        <meta property="og:image" content="https://i.imgur.com/33rSf59.png" />
+      </>
+    );
+  }
+  
